4b9161f076176f8284ff704f19bece08ee4cd4ac
ShakiraMorium <shakiramorium@gmail.com>
2023-12-23 02:46:53 +0600

    firebase config added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log (import.meta.env.VITE_PASS)
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBPiZnohdXEmUNSoVnTHhiMXkKFWobej4s",
-  authDomain: "event-management-f88a6.firebaseapp.com",
-  projectId: "event-management-f88a6",
-  storageBucket: "event-management-f88a6.appspot.com",
-  messagingSenderId: "879859519671",
-  appId: "1:879859519671:web:3231d3263260eff156bd32"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APIID,
 };
 
 // Initialize Firebase
